078fa92e9203ed259bc6d97d917cc9017c7f48f2
Ughan P <ughan2007@gmail.com>
2026-08-27 14:10:04 +0530

    Time: 0 ms (100%), Space: 42 MB (61.84%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int integerReplacement(int n) {
+        long num=n;
+        int c=0;
+        while(num!=1){
+            if(num%2==0){
+                num/=2;
+            }
+            else{
+                if(num==3 || num%4==1){
+                    num--;
+                }
+                else{
+                    num++;
+                }
+            }
+            c++;
+        }
+        return c;
+    }
+}
