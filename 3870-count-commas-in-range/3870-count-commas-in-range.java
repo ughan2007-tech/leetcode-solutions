353c781536e6802322ee97fdcdd597fbfd9d0f74
Ughan P <ughan2007@gmail.com>
2026-09-08 17:54:26 +0530

    Time: 1 ms (83.65%), Space: 42.7 MB (22.48%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,11 +1,5 @@
 class Solution {
     public int countCommas(int n) {
-        int c=0;
-        for(int i=0;i<=n;i++){
-            if(i>=1000){
-                c++;
-            }
-        }
-        return c;
+        return Math.max(0,n-999);
     }
 }
