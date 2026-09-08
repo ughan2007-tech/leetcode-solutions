f47cee45a798cff9ea66d41f053b0e795f907a73
Ughan P <ughan2007@gmail.com>
2026-09-09 01:22:42 +0530

    Time: 9 ms (16.47%), Space: 46.5 MB (80.8%) - LeetHub

---
+++ 2716-minimize-string-length/2716-minimize-string-length.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimizedStringLength(String s) {
+         HashMap<Character,Integer>map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)+1);
+            }
+            else{
+                map.put(ch,1);
+            }
+        }
+        return map.size();
+    }
+}
